b1bdc2a3f2442e479d97882641ecdeb1320d04e7
Swati Kumari Singh <swati.ksingh13@gmail.com>
2024-07-02 21:37:02 +0530

    Add executors callables and adder subtractor problem code

--- src/main/java/in/scaler/lld1200624/lld1class7/executors/Client.java
+++ src/main/java/in/scaler/lld1200624/lld1class7/executors/Client.java
@@ -8,7 +8,7 @@ import java.util.concurrent.Executors;
 public class Client {
     public static void main(String[] args) {
 
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        ExecutorService executorService = Executors.newFixedThreadPool(10);// here we decide no. of threads in 1 pool in 1 executorService
 
         for (int i = 1; i < 100; i++) {
             NumberPrinter numberPrinter = new NumberPrinter(i);
