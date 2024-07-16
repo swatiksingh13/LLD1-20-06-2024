f40e3e427b37114666c396c60acf2f37c92c2f94
Swati Kumari Singh <swati.ksingh13@gmail.com>
2024-07-16 23:26:38 +0530

    Added Comments

--- src/main/java/in/scaler/lld1200624/lld1class13/exceptions/Client.java
+++ src/main/java/in/scaler/lld1200624/lld1class13/exceptions/Client.java
@@ -36,9 +36,10 @@ private static void demoTryCatch() {
         int b = scanner.nextInt();
 
         System.out.println(a/b);
-        System.exit(-1);
-        return;
+        System.exit(-1);  //kill the application , finally  will not execute
+        return;   //still finally will execute
     }
+// if try logic is good then not catch run as not exception if there is exception catch print as we catch the exception
 
 //        catch (FileNotFoundException e) {
 //            System.out.println("FileNotFoundException " + e.getMessage());
@@ -55,9 +56,13 @@ private static void demoTryCatch() {
     catch (NullPointerException abc) {
         System.out.println("NullPointerException " + abc.getMessage());
     }
+    // at the end  as Exception is parent
+    //as if any  exception  not match then catch Exception e which is a parent
+    //coz else every one will become redundant they are children
     catch (Exception e) {
         System.out.println("Exception " + e.getMessage());
     }
+    //sout can not be in middle after finally
     finally {
         System.out.println("Finally block");
         System.out.println("Finally: Some more business logic of cleaning resources here, e.g DB connection , network connection etc");
